56cb0c6c31596cf16453ae822fe24f17dc5a508e
John MacFarlane <jgm@berkeley.edu>
2016-01-04 07:34:13 -0800

    Better error handling for nonexistent file in include filter.

--- filters/include.lua
+++ filters/include.lua
@@ -9,6 +9,7 @@ return function(doc, meta, to)
                                        'include="([^"]*)"')
       if includefile then
         local f = io.open(includefile, 'r')
+        assert(f, "Could not open file " .. includefile)
         local contents = f:read("*all")
         -- set contents of code block:
         node_set_literal(cur, contents)
